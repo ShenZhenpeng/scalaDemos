081c570dbfc8d70c15f5cd0c7a0d5d9b8767fc26
ShenZhenpeng <984280514@qq.com>
2016-08-28 23:29:42 +0800

    scala test 01

---
+++ src/test.scala
@@ -0,0 +1,9 @@
+/**
+  * Created by szp_s on 2016/8/28.
+  */
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    val str : String = "test";
+    println(str);
+  }
+}
---
+++ src/test01DataType.scala
@@ -0,0 +1,11 @@
+/**
+  * Created by szp_s on 2016/8/28.
+  */
+
+object Test {
+  def main(args: Array[String]): Unit = {
+    println("Hello World" +
+      "" +
+      "");
+  }
+}
